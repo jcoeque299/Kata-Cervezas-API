9e118cafb96ff3f2882fbb073e8f68d4b8997a18
Judith <jcoeque299@g.educaand.es>
2023-10-18 14:15:28 +0200

    first commit

---
+++ src/main/java/com/example/KataCervezas/KataCervezasApplication.java
@@ -0,0 +1,13 @@
+package com.example.KataCervezas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataCervezasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataCervezasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KataCervezas/KataCervezasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KataCervezas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataCervezasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
